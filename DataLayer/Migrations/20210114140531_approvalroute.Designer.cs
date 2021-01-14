2af0d34659526da27d02f1ee8aaf8669e4a96a2a
Daketima <teslimbakare@live.com>
2021-01-14 17:05:32 +0100

    commit changes migration

--- DataLayer/Migrations/20210114140531_approvalroute.Designer.cs
+++ DataLayer/Migrations/20210114140531_approvalroute.Designer.cs
@@ -4,14 +4,16 @@ using DataLayer.DataContext;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace DataLayer.Migrations
 {
     [DbContext(typeof(CustomContext))]
-    partial class CustomContextModelSnapshot : ModelSnapshot
+    [Migration("20210114140531_approvalroute")]
+    partial class approvalroute
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ DataLayer/Migrations/20210114140531_approvalroute.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DataLayer.Migrations
+{
+    public partial class approvalroute : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "InitiatedByBranchCode",
+                table: "Payment",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "InitiatedBy",
+                table: "Assessment",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "InitiatedByBranchCode",
+                table: "Assessment",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "InitiatedByBranchCode",
+                table: "Payment");
+
+            migrationBuilder.DropColumn(
+                name: "InitiatedBy",
+                table: "Assessment");
+
+            migrationBuilder.DropColumn(
+                name: "InitiatedByBranchCode",
+                table: "Assessment");
+        }
+    }
+}
